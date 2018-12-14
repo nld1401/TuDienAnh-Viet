bd012295581339eb8f7973784cbe967b845e55bb
Nguyễn Mạnh Tùng <17521236@gm.uit.edu.vn>
2018-12-14 18:53:02 +0700

    Create AnhViet.sql
    chuyển từ txt sang sql

---
+++ CoverSQL-to-XML/AnhViet.sql
@@ -0,0 +1,15 @@
+﻿CREATE DATABASE AnhViet
+CREATE TABLE DictData
+(
+word NTEXT NOT NULL,
+Phatam NTEXT NOT NULL,
+mean NTEXT NOT NULL
+)
+---- đường dẫn tới DictData.txt
+BULK INSERT dbo.DictData01 FROM 'E:\GitHub\TuDienAnh-Viet\TuDienAnh-Viet\CoverSQL-to-XML\DictData.txt' -- đường dẫn tới DictData.txt
+WITH 
+(
+FIELDTERMINATOR = '/', 
+ROWTERMINATOR   = '@',
+DATAFILETYPE = 'WIDECHAR')
+SELECT * FROM dbo.DictData
