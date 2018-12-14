bb69e8835e555ff50fa3c39687c91b84661572ea
Nguyễn Lương Duy <45115891+nld1401@users.noreply.github.com>
2018-12-14 15:29:29 +0700

    Create DictData.cs

---
+++ TuDienAnh-Viet-master/WindowsFormsApplication1/WindowsFormsApplication1/DictData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class DictData
+    {
+        private string key;
+
+        public string Key
+        {
+            get
+            {
+                return key;
+            }
+
+            set
+            {
+                key = value;
+            }
+        }
+        private string mean;
+
+        public string Mean
+        {
+            get
+            {
+                return mean;
+            }
+
+            set
+            {
+                mean = value;
+            }
+        }
+        private string explain;
+
+        public string Explain
+        {
+            get
+            {
+                return explain;
+            }
+
+            set
+            {
+                explain = value;
+            }
+        }
+
+       
+
+       
+    }
+}
