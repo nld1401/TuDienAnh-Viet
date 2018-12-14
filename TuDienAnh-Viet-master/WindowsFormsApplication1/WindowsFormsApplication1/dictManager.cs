ad637d1b55e3adc30356c80801bc076fd6d07068
Nguyễn Lương Duy <45115891+nld1401@users.noreply.github.com>
2018-12-14 15:29:38 +0700

    Create dictManager.cs

---
+++ TuDienAnh-Viet-master/WindowsFormsApplication1/WindowsFormsApplication1/dictManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace WindowsFormsApplication1
+{
+    public class DictManager
+    {
+        #region properties
+        private string filePath = "data.xml";
+
+        private DictItem items;
+
+        public DictItem Items
+        {
+            get { return items; }
+            set { items = value; }
+        }
+        #endregion
+
+        #region methods
+        public DictManager()
+        {
+            items = (DictItem)DeserializeFromXML(filePath);
+        }
+
+        public void LoadDataToCombobox(ComboBox combo)
+        {
+            combo.DataSource = items.Items;
+        }
+
+
+        public void Serialize()
+        {
+            SerializeToXML(Items, filePath);
+        }
+
+        private void SerializeToXML(object data, string filePath)
+        {
+            FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            XmlSerializer sr = new XmlSerializer(typeof(DictItem));
+
+            sr.Serialize(fs, data);
+
+            fs.Close();
+        }
+
+        public object DeserializeFromXML(string filePath)
+        {
+            FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            XmlSerializer sr = new XmlSerializer(typeof(DictItem));
+
+            object obj = sr.Deserialize(fs);
+
+            fs.Close();
+
+            return obj;
+        }
+
+        #endregion
+    }
+}
