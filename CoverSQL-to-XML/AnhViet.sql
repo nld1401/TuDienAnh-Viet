e2ff9429f070440962e60f7890dbfab9442538df
Nguyễn Mạnh Tùng <17521236@gm.uit.edu.vn>
2018-12-21 17:58:51 +0700

    Update AnhViet.sql

--- CoverSQL-to-XML/AnhViet.sql
+++ CoverSQL-to-XML/AnhViet.sql
@@ -1,15 +1,17 @@
 ﻿CREATE DATABASE AnhViet
+USE AnhViet
 CREATE TABLE DictData
 (
 word NTEXT NOT NULL,
-Phatam NTEXT NOT NULL,
 mean NTEXT NOT NULL
 )
 ---- đường dẫn tới DictData.txt
-BULK INSERT dbo.DictData01 FROM 'E:\GitHub\TuDienAnh-Viet\TuDienAnh-Viet\CoverSQL-to-XML\DictData.txt' -- đường dẫn tới DictData.txt
+BULK INSERT dbo.DictData FROM 'E:\GitHub\TuDienAnh-Viet\TuDienAnh-Viet\CoverSQL-to-XML\DictData.txt' -- đường dẫn tới DictData.txt
 WITH 
 (
 FIELDTERMINATOR = '/', 
 ROWTERMINATOR   = '@',
 DATAFILETYPE = 'WIDECHAR')
 SELECT * FROM dbo.DictData
+
+DROP DATABASE AnhViet
