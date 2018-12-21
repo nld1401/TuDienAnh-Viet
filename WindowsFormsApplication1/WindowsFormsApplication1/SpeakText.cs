dda48e3131deb1780c347039d276cb984fe52da0
Nguyễn Mạnh Tùng <17521236@gm.uit.edu.vn>
2018-12-21 17:22:11 +0700

    Create SpeakText.cs

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/SpeakText.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public class SpeakText
+    {
+        private WebBrowser wbweb;
+public WebBrowser Wbweb
+        {
+            get
+            {
+                return wbweb;
+            }
+
+            set
+            {
+                wbweb = value;
+            }
+        }
+        public SpeakText(WebBrowser wb)
+        {
+            this.wbweb = wb;
+        }
+        private void settext(string data)
+        {
+            wbweb.Document.GetElementById("cdo-search-input").InnerText = data;
+            
+            
+            
+        }
+        private void speaktxt()
+        {
+             HtmlElement element = wbweb.Document.GetElementById("cdo-search-input");
+            element.InvokeMember("click");
+            
+        }
+       
+        public  void Speak(string data)
+        {
+            settext(data);
+           speaktxt();
+                        
+        }
+    }
+}
+ 
